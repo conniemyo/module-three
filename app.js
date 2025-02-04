8c1cb69ed85a46d37a58081c566bd5efeee8b6ce
conniemyo <connieyoungxo@gmail.com>
2025-02-04 12:51:27 -0600

    changed functions

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 // FUNCTION #1: to roll
 function rollDie(sides) {
-    return Math.floor(Math.random() * sides) + 1;
+    let roll = Math.random() * sides;
+    let wholeNumber = Math.floor(roll) + 1;
+    return wholeNumber;
 }
+
 // FUNCTION #2: generate "roll" message
-function getDiceMessage(roll) {
-    return `You rolled a ${roll}!`;
+function showRollMessage(roll) {
+    return "You rolled a " + roll + "!";
 }
+
+// FUNCTION #3: check if roll = lucky number
+function checkLuckyNumber(roll, luckyNumber) {
+    if (roll == luckyNumber) {
+        return "You got the Lucky Number!";
+    } else {
+        return "Try again!";
+    }
+}
+
+// FUNCTION #4: dice
+let dice = {
+    sides: 6,
+    lastRoll: 1,
+
+    roll: function() {
+        this.lastRoll = rollDie(this.sides);
+        return this.lastRoll;
+    }
+};
