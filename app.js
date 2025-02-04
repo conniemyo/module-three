9e05c2f701c7e334327351565eb399c66fd10640
conniemyo <connieyoungxo@gmail.com>
2025-02-04 15:07:58 -0600

    code organization

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-// FUNCTION #1: to roll
-function rollDice(sides) {
+// function #1: to roll
+function rollDice(sides){
     let roll = Math.random() * sides;
     let wholeNumber = Math.floor(roll) + 1;
     return wholeNumber;
 }
 
-// FUNCTION #2: generate "roll" message
-function showRollMessage(roll) {
+// function #2: generate "roll" message
+function showRollMessage(roll){
     return "You rolled a... " + roll + "!";
 }
 
-// FUNCTION #3: check if roll = lucky number
-function checkLuckyNumber(roll, luckyNumber) {
-    if (roll == luckyNumber) {
+// function #3: check if roll = lucky number
+function checkLuckyNumber(roll, luckyNumber){
+    if (roll == luckyNumber){
         return "You got the Lucky Number!";
     } else {
         return "Roll again!";
     }
 }
 
-// OBJECT: dice
+// object: dice
 let dice = {
     sides: 6,
     lastRoll: 1,
 
-    // METHOD: store the result
-    roll: function() {
+    // method: store the result
+    roll: function(){
         this.lastRoll = rollDice(this.sides);
         return this.lastRoll;
     }
 };
 
-// EVENT: button
-document.getElementById("rollDiceBtn").addEventListener("click", function() {
+// event: button
+document.getElementById("rollDiceBtn").addEventListener("click", function(){
     let luckyNumber = prompt("Pick a lucky number (1-6):");
     luckyNumber = Number(luckyNumber);
 
